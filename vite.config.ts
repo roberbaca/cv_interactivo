3b7e29a4c35f550a64a6dbce75af953cd79cd63e
roberbaca <roberto.nicolas.baca@gmail.com>
2025-12-02 21:43:26 -0300

    Fix Vite base for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: '/', // 👈 IMPORTANTÍSIMO PARA VERCEL
-
     server: {
       port: 3000,
       host: '0.0.0.0',
@@ -19,7 +17,8 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, './'),
       },
-    },
+    }
   };
 });
+
 
